87da64b43a4ca41fb5afa6cadb0ebf65b600d075
fishbowlsoftware <nik@fishbowlllc.com>
2022-03-07 09:57:09 -0500

    display readios horizontally for PKCE method

--- OidcDebugger/Startup.cs
+++ OidcDebugger/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.HttpOverrides;
 using System.Net;
+using System.IO;
 
 namespace OidcDebugger
 {
@@ -66,6 +67,7 @@ namespace OidcDebugger
 
                 app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
                 {
+                    ProjectPath = Path.Combine(env.ContentRootPath, @".."),
                     HotModuleReplacement = true,
                 });
             }
