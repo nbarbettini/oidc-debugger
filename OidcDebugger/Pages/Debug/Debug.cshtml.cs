0bff7cda9292f339874f1679976e90c7bef34d42
nbarbettini <nathanaelb@gmail.com>
2024-09-07 20:54:12 -0700

    Un-ignore Pages/Debug

---
+++ OidcDebugger/Pages/Debug/Debug.cshtml.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OidcDebugger.Extensions;
+
+namespace OidcDebugger.Pages.Debug;
+
+public class DebugModel : PageModel
+{
+    public string Method { get; set; } = "GET";
+
+    public string? Referer { get; set; }
+
+    public KeyValuePair<string, string?>[] Form { get; set; } = [];
+
+    public void OnGet()
+    {
+        CopyValuesFromRequest();
+    }
+
+    public void OnPost()
+    {
+        CopyValuesFromRequest();
+    }
+
+    private void CopyValuesFromRequest()
+    {
+        Method = Request.Method;
+
+        Referer = HttpContext.Request.Headers.Referer;
+
+        Form = Request.HasFormContentType
+            ? Request.Form.Flatten().ToArray()
+            : [];
+    }
+}
